28ee5777859a3d516defa26ae1a4046ab8c2f6fd
Cedric Tallichet <sirleamont@live.com>
2017-07-04 10:00:27 +0200

    Initial commit

---
+++ MyStrings/ResourceProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+
+namespace MyStrings
+{
+    public class ResourcesProvider
+    {
+        private static ResourcesProvider _instance;
+
+        public static ResourcesProvider Instance => (_instance ?? (_instance = new ResourcesProvider()));
+
+        public ResourcesProvider()
+        {
+            
+            this.Resources = new ResourceManager(typeof(Resource));
+        }
+
+        public ResourceManager Resources { get; }
+    }
+}
